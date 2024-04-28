a33752e8ddffbd34efe5cd7cca8a2d58fb1fec6c
Andresf-Asprilla <andresf.asprilla1414@gmail.com>
2024-04-28 13:01:48 -0500

    taller 3 parte 1

--- Taller_unidad3/ArchivoPrueba_lastrun.py
+++ Taller_unidad3/ArchivoPrueba_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.1),
-    on abril 28, 2024, at 12:41
+    on abril 28, 2024, at 12:57
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -375,15 +375,6 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         text='',
         font='Open Sans',
         pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
-        color='white', colorSpace='rgb', opacity=None, 
-        languageStyle='LTR',
-        depth=0.0);
-    
-    # --- Initialize components for Routine "FInalizacion" ---
-    FinPrueba = visual.TextStim(win=win, name='FinPrueba',
-        text='',
-        font='Open Sans',
-        pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0, 
         color='white', colorSpace='rgb', opacity=None, 
         languageStyle='LTR',
         depth=0.0);
@@ -801,102 +792,6 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
             thisSession.sendExperimentData()
     # completed 1.0 repeats of 'Prueba'
     
-    
-    # --- Prepare to start Routine "FInalizacion" ---
-    continueRoutine = True
-    # update component parameters for each repeat
-    thisExp.addData('FInalizacion.started', globalClock.getTime(format='float'))
-    FinPrueba.setText('Te informamos que la adquisición ha finalizado. ¡Muchas gracias por participar!\n ')
-    # keep track of which components have finished
-    FInalizacionComponents = [FinPrueba]
-    for thisComponent in FInalizacionComponents:
-        thisComponent.tStart = None
-        thisComponent.tStop = None
-        thisComponent.tStartRefresh = None
-        thisComponent.tStopRefresh = None
-        if hasattr(thisComponent, 'status'):
-            thisComponent.status = NOT_STARTED
-    # reset timers
-    t = 0
-    _timeToFirstFrame = win.getFutureFlipTime(clock="now")
-    frameN = -1
-    
-    # --- Run Routine "FInalizacion" ---
-    routineForceEnded = not continueRoutine
-    while continueRoutine and routineTimer.getTime() < 1.0:
-        # get current time
-        t = routineTimer.getTime()
-        tThisFlip = win.getFutureFlipTime(clock=routineTimer)
-        tThisFlipGlobal = win.getFutureFlipTime(clock=None)
-        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
-        # update/draw components on each frame
-        
-        # *FinPrueba* updates
-        
-        # if FinPrueba is starting this frame...
-        if FinPrueba.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
-            # keep track of start time/frame for later
-            FinPrueba.frameNStart = frameN  # exact frame index
-            FinPrueba.tStart = t  # local t and not account for scr refresh
-            FinPrueba.tStartRefresh = tThisFlipGlobal  # on global time
-            win.timeOnFlip(FinPrueba, 'tStartRefresh')  # time at next scr refresh
-            # add timestamp to datafile
-            thisExp.timestampOnFlip(win, 'FinPrueba.started')
-            # update status
-            FinPrueba.status = STARTED
-            FinPrueba.setAutoDraw(True)
-        
-        # if FinPrueba is active this frame...
-        if FinPrueba.status == STARTED:
-            # update params
-            pass
-        
-        # if FinPrueba is stopping this frame...
-        if FinPrueba.status == STARTED:
-            # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > FinPrueba.tStartRefresh + 1-frameTolerance:
-                # keep track of stop time/frame for later
-                FinPrueba.tStop = t  # not accounting for scr refresh
-                FinPrueba.tStopRefresh = tThisFlipGlobal  # on global time
-                FinPrueba.frameNStop = frameN  # exact frame index
-                # add timestamp to datafile
-                thisExp.timestampOnFlip(win, 'FinPrueba.stopped')
-                # update status
-                FinPrueba.status = FINISHED
-                FinPrueba.setAutoDraw(False)
-        
-        # check for quit (typically the Esc key)
-        if defaultKeyboard.getKeys(keyList=["escape"]):
-            thisExp.status = FINISHED
-        if thisExp.status == FINISHED or endExpNow:
-            endExperiment(thisExp, win=win)
-            return
-        
-        # check if all components have finished
-        if not continueRoutine:  # a component has requested a forced-end of Routine
-            routineForceEnded = True
-            break
-        continueRoutine = False  # will revert to True if at least one component still running
-        for thisComponent in FInalizacionComponents:
-            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
-                continueRoutine = True
-                break  # at least one component has not yet finished
-        
-        # refresh the screen
-        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
-            win.flip()
-    
-    # --- Ending Routine "FInalizacion" ---
-    for thisComponent in FInalizacionComponents:
-        if hasattr(thisComponent, "setAutoDraw"):
-            thisComponent.setAutoDraw(False)
-    thisExp.addData('FInalizacion.stopped', globalClock.getTime(format='float'))
-    # using non-slip timing so subtract the expected duration of this Routine (unless ended on request)
-    if routineForceEnded:
-        routineTimer.reset()
-    else:
-        routineTimer.addTime(-1.000000)
-    thisExp.nextEntry()
     
     # mark experiment as finished
     endExperiment(thisExp, win=win)
